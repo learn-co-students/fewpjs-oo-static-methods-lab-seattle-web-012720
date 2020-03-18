0c2e65fb6915c3490908b7b6a1e4f430ed57ef4e
dianakw8591 <dianakw@gmail.com>
2020-03-18 16:11:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9'-\s]/g, '');
+  }
+
+  static titleize(string) {
+    const array = string.split(" ");
+    const first = this.capitalize(array[0])
+    const newArray = array.slice(1).map(word => {
+      switch (word) {
+        case "the":
+        case "a":
+        case "an":
+        case "but":
+        case "of":
+        case "and":
+        case "for":
+        case "at":
+        case "by":
+        case "from":
+          return word;
+          break;
+        default:
+          return this.capitalize(word);
+      } 
+    });
+    return first + " " + newArray.join(" ");
+  }
 }
